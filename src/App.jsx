7006e24e3e58d4eab24c2361463daa3c9949cf1c
Dulmi Chamathka <chamathkawijesekara23@gmail.com>
2025-09-22 12:24:29 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Navbar from "./Components/Navbar";
+import Cart from "./Pages/Cart";
+import axios from "axios";
+
+const App = () => {
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(async (pos) => {
+      const { latitude, longitude } = pos.coords;
+      console.log("Lat:", latitude, "Lng:", longitude);
+
+      // Use CORS proxy to bypass the CORS issue
+      const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+      const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`;
+
+      try {
+        const response = await axios.get(proxyUrl + url);
+        console.log(response.data); // The location data
+      } catch (error) {
+        console.error("Error fetching location:", error);
+      }
+    });
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,112 @@
+import { MapPin } from "lucide-react";
+import { Link, NavLink } from "react-router-dom";
+import { FaCaretDown } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [openDropdown, setOpenDropdown] = useState(false);
+  return (
+    <div className="bg-white py-3 shadow-2xl">
+      <div className="max-w-6xl mx-auto flex justify-between items-center">
+        {/* logo section */}
+        <div className="flex gap-7  items-center">
+          <Link to={"/"}>
+            <h1 className="font-bold text-3xl">
+              <span className="text-red-500 font-serif">Z</span>apro
+            </h1>
+          </Link>
+          <div className="flex gap-1 cursor-pointer text-gray-700 items-center">
+            <MapPin className="text-red-500" />
+            <span className="font-semibold">
+              {location ? <div></div> : "Add address"}
+            </span>
+            <FaCaretDown />
+          </div>
+          {openDropdown ? <div></div> : null}
+        </div>
+
+        {/* menu section */}
+
+        <nav className="flex gap-7 items-center">
+          <ul className="flex gap-7 items-center text-xl font-semibold">
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Home</li>
+            </NavLink>
+            <NavLink
+              to={"/products"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Products</li>
+            </NavLink>
+            <NavLink
+              to={"/about"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>About</li>
+            </NavLink>
+            <NavLink
+              to={"/contact"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Contact</li>
+            </NavLink>
+          </ul>
+          <Link to={"/cart"} className="relative">
+            <IoCartOutline className="h-7 w-7" />
+            <span className="bg-red-500 rounded-full px-2 absolute -top-3 -right-3 text-white">
+              0
+            </span>
+          </Link>
+          <div>
+            <SignedOut>
+              <SignInButton mode="modal">
+                <button className="bg-red-500 text-white px-3 py-1 rounded-md">
+                  Sign in
+                </button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
